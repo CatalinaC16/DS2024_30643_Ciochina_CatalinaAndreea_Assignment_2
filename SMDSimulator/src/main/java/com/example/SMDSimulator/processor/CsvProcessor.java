f44d3d883012d3477b8df4cf5ad0c759db9fba42
CatalinaC16 <catalina.ciochina16@gmail.com>
2024-11-14 23:59:17 +0200

    Simulator -update

--- SMDSimulator/src/main/java/com/example/SMDSimulator/processor/CsvProcessor.java
+++ SMDSimulator/src/main/java/com/example/SMDSimulator/processor/CsvProcessor.java
@@ -6,10 +6,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -20,12 +17,13 @@ public class CsvProcessor {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
+    private static final long timeIncrement = 60000;//0
+
     public void readAndSendData(String deviceId) {
         try (InputStream inputStream = getClass().getResourceAsStream("/sensor.csv");
              BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
 
             String line;
-            long timeMillis = System.currentTimeMillis();
             while ((line = reader.readLine()) != null) {
                 if (line.startsWith("#")) {
                     continue;
@@ -33,15 +31,31 @@ public class CsvProcessor {
 
                 float measurementValue = Float.parseFloat(line);
                 ObjectNode jsonNode = objectMapper.createObjectNode();
-                jsonNode.put("timestamp", timeMillis += 1000 * 60 * 5);
+                jsonNode.put("timestamp", computeTime(deviceId));
                 jsonNode.put("device_id", deviceId);
                 jsonNode.put("measure", measurementValue);
                 String message = jsonNode.toString();
 
                 messageProducer.sendMessage(message);
-                TimeUnit.MILLISECONDS.sleep(1000);
+                TimeUnit.MILLISECONDS.sleep(timeIncrement);
             }
         } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static long computeTime(String id) {
+        long timeMillis = System.currentTimeMillis();
+        long timer = 0;
+        timer += timeIncrement;
+        saveDateToFile(id + ".txt", timeMillis + timer);
+        return timeMillis + timer;
+    }
+
+    private static void saveDateToFile(String fileName, long date) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(fileName))) {
+            writer.println(date);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
